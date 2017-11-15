4d9af812192aaa5f455a058b1c93206fcaf27eba
Mike Mirzayanov <mirzayanovmr@gmail.com>
2017-11-15 06:10:52 +0300

    Linux binary

--- java/riorita/native/make.sh
+++ java/riorita/native/make.sh
@@ -1,1 +1,5 @@
-g++ -std=c++14 -fPIC -O2 -g -static -shared -o riorita_engine.so -I /usr/lib/jvm/java-1.8.0/include -I /usr/lib/jvm/java-1.8.0/include/linux compact.cpp riorita_engine.cpp -lboost_system -lboost_thread -lboost_filesystem -lboost_program_options -lpthread -lsnappy
+mv -f /usr/lib/gcc/x86_64-redhat-linux/7/crtbeginT.o /usr/lib/gcc/x86_64-redhat-linux/7/crtbeginT.o.tmp
+cp -f /usr/lib/gcc/x86_64-redhat-linux/7/crtbeginS.o /usr/lib/gcc/x86_64-redhat-linux/7/crtbeginT.o
+g++ -std=c++14 -fPIC -O2 -g -static -static-libgcc -static-libstdc++ -shared -o riorita_engine.so -I /usr/lib/jvm/java-1.8.0/include -I /usr/lib/jvm/java-1.8.0/include/linux compact.cpp riorita_engine.cpp -lboost_system -lboost_thread -lboost_filesystem -lboost_program_options -lpthread -lsnappy
+mv -f /usr/lib/gcc/x86_64-redhat-linux/7/crtbeginT.o.tmp /usr/lib/gcc/x86_64-redhat-linux/7/crtbeginT.o
+cp -f riorita_engine.so ../src/main/files/riorita_engine.so
