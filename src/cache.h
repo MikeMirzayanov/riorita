70cab54832055b5590938fe33abc4ddb8580cab2
mikemirzayanov <mirzayanovmr@gmail.com>
2026-06-05 21:03:20 +0300

    Summarize cache metrics periodically

--- src/cache.h
+++ src/cache.h
@@ -14,9 +14,20 @@ private:
     std::map<size_t, std::string> keysByTimestamp;
     std::map<std::string, size_t> timestampsByKey;
     std::unordered_map<std::string, std::string> values;
+    long long lastSummaryMillis = 0;
+    unsigned long long getHitCount = 0;
+    unsigned long long getMissCount = 0;
+    unsigned long long hasHitCount = 0;
+    unsigned long long hasMissCount = 0;
+    unsigned long long putCount = 0;
+    unsigned long long skippedTooLargeCount = 0;
+    unsigned long long evictionCount = 0;
+    unsigned long long evictedBytes = 0;
 
     void renewTimestamp(const std::string& key);
     void removeOutdated();
+    void maybeLogSummary();
+    void resetSummaryCounters();
 
 public:
     static size_t MAX_CACHE_ENTRY_SIZE;
